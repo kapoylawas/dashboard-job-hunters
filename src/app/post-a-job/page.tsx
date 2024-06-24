b2aef0db4f3adbef91b7f6c41261a6f43045171c
kapoylawas <arif.sangga@gmail.com>
2024-06-24 13:11:21 +0700

    update add skill and ck editor

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -3,13 +3,38 @@
 import { jobFormSchema } from "@/lib/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowLeftIcon } from "lucide-react";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import FieldInput from "@/components/organisms/FieldInput";
+import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { JOBTYPES } from "@/constans";
+import InputSkills from "@/components/organisms/InputSkills";
+import CKEditor from "@/components/organisms/CKEditor";
 
 interface PostJobPageProps {}
 
 const PostJobPage: FC<PostJobPageProps> = ({}) => {
+  const [editorLoaded, setEditorLoaded] = useState<boolean>(false);
+
   const form = useForm<z.infer<typeof jobFormSchema>>({
     resolver: zodResolver(jobFormSchema),
     defaultValues: {
@@ -21,6 +46,10 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
     console.log(val);
   };
 
+  useEffect(() => {
+    setEditorLoaded(true);
+  }, []);
+
   return (
     <div>
       <div className="inline-flex items-center gap-2 cursor-pointer hover:text-primary">
@@ -29,8 +58,200 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
       </div>
       <div className="my-5">
         <div className="text-lg font-semibold">Basic Information</div>
-        <div className="text-gray-400">List out your top perks and benefit.</div>
+        <div className="text-gray-400">
+          List out your top perks and benefit.
+        </div>
       </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-5 space-y-6 pt-6"
+        >
+          <FieldInput
+            title="Job title"
+            subtitle="Job title must be  describe one position"
+          >
+            <FormField
+              control={form.control}
+              name="roles"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="Sofware Enginer"
+                      {...field}
+                      className="w-[450px]"
+                    />
+                  </FormControl>
+                  <FormDescription>At least 80 characters</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+          <hr></hr>
+          <FieldInput
+            title="Type of Employment"
+            subtitle="You can select multiple type of employment"
+          >
+            <FormField
+              control={form.control}
+              name="jobType"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      {JOBTYPES.map((item: string, i: number) => (
+                        <FormItem
+                          key={item + i}
+                          className="flex items-center space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <RadioGroupItem value={item} />
+                          </FormControl>
+                          <FormLabel className="font-normal">{item}</FormLabel>
+                        </FormItem>
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+          <hr></hr>
+          <FieldInput
+            title="Salary"
+            subtitle="Please specify the estimated salary  range for the  role"
+          >
+            <div className="w-[450px] flex flex-row justify-between items-center">
+              <FormField
+                control={form.control}
+                name="salaryForm"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="$100" {...field} className="w-full" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <span className="text-center">To</span>
+              <>
+                <FormField
+                  control={form.control}
+                  name="salaryTo"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          placeholder="$1000"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            </div>
+          </FieldInput>
+          <hr></hr>
+          <FieldInput
+            title="categories"
+            subtitle="You can select multiple job categories"
+          >
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Select Job Categories</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-[450px]">
+                        <SelectValue placeholder="Select Job Categories" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="m@example.com">
+                        m@example.com
+                      </SelectItem>
+                      <SelectItem value="m@google.com">m@google.com</SelectItem>
+                      <SelectItem value="m@support.com">
+                        m@support.com
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+          <hr />
+          <FieldInput
+            title="Required Skills"
+            subtitle="Add required skills for the job"
+          >
+            <InputSkills form={form} />
+          </FieldInput>
+          <hr />
+          <FieldInput
+            title="Job Description"
+            subtitle="Job titles must be describe one position"
+          >
+            <CKEditor
+              form={form}
+              name="jobDescription"
+              editorLoaded={editorLoaded}
+            />
+          </FieldInput>
+          <hr />
+          <FieldInput
+            title="Responsibility"
+            subtitle="Outline the core responsibilities of the position"
+          >
+            <CKEditor
+              form={form}
+              name="responsibilty"
+              editorLoaded={editorLoaded}
+            />
+          </FieldInput>
+          <hr />
+          <FieldInput
+            title="Who are you"
+            subtitle="Add your prefered candidates qualifications"
+          >
+            <CKEditor
+              form={form}
+              name="whoYouAre"
+              editorLoaded={editorLoaded}
+            />
+          </FieldInput>
+          <FieldInput
+            title="Nice To Haves"
+            subtitle="Add nice to have skills and qualifications"
+          >
+            <CKEditor
+              form={form}
+              name="niceToHaves"
+              editorLoaded={editorLoaded}
+            />
+          </FieldInput>
+        </form>
+      </Form>
     </div>
   );
 };
